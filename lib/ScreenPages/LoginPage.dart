73ff47caaa3982f451549c31b6390b39cb865f26
muhammedbadusha <muhammedbadusha8888@gmail.com>
2022-05-01 20:47:50 +0530

    lastlogi

--- lib/ScreenPages/LoginPage.dart
+++ lib/ScreenPages/LoginPage.dart
@@ -11,28 +11,26 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  ///////
-  var _key=GlobalKey<FormState>();
-  _submit() {
-
-      if (_key.currentState!.validate()) {
-return ;
-      } else {
-        return 'something wrong';
-      }
-
-  }
-  //////////
 
 
   //bool isPhone(String input) => RegExp(r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$')
 
+  TextEditingController passwordcontroller=TextEditingController();
+  TextEditingController emailcontroller=TextEditingController();
+
+  GlobalKey<FormState> _key = new GlobalKey();
+ // String formFieldValue;
+  _submit() {
+    if (_key.currentState!.validate()) {
+      return ;
+    } else {
+      return 'something wrong';
+    }
+  }
   @override
 
   Widget build(BuildContext context) {
-   String _email;
+
     return Scaffold(
 
       body: Padding(
@@ -45,52 +43,87 @@ return ;
             color: Colors.greenAccent,
             elevation: 3.8,
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  ClipPath(
-                    clipper: WaveClipperTwo(reverse: false),
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * .40,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 13),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const[
-                            Text(
-                              'Welcome',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w700,
-                                  letterSpacing: 1),
-                            ),
-                            Text(
-                              'Back',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w700,
-                                  letterSpacing: 1),
-                            ),
-                          ],
+              child: Form(
+                key: _key,
+                child: Column(
+                  children: [
+                    ClipPath(
+                      clipper: WaveClipperTwo(reverse: false),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * .40,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 13),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const[
+                              Text(
+                                'Welcome',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.w700,
+                                    letterSpacing: 1),
+                              ),
+                              Text(
+                                'Back',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.w700,
+                                    letterSpacing: 1),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  //////1st email
-                  Form(
-                    key: _key,
-                      child: Container(
-                        //height: MediaQuery.of(context).size.height *.27,
+                    //////1st email
+                    Container(
+                      //height: MediaQuery.of(context).size.height *.27,
 
-                        child: Column(
-                    children: [
+                      child: Column(
+                      children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 14, 0, 0),
+                          child: Icon(
+                            Icons.lock,
+                            size: 17,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height * .10,
+                          width: MediaQuery.of(context).size.width * .80,
+                          padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
+                          child: TextFormField(
+                            controller: emailcontroller,
+                            validator: ( value) {
+                              if(value==""){
+                                return "emial Can't Empty";
+                              } if(RegExp(z).hasMatch(value)){
+                                return "Please enter validate email";
+                              }
+                            },
+                            autofillHints: [AutofillHints.email],
+                            cursorColor: Colors.red,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: "Email",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                      ////////////2nd
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -103,114 +136,73 @@ return ;
                             ),
                           ),
                           Container(
-                            height: MediaQuery.of(context).size.height * .10,
                             width: MediaQuery.of(context).size.width * .80,
-                            padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
+                            padding: EdgeInsets.fromLTRB(10, 30, 10, 05),
                             child: TextFormField(
-
-                              validator: (dynamic value){
-                                if(value.isEmpty){
-                                  return 'Please enter email';
+                              validator: (val) {
+                                if(val ==""){
+                                  return "password Can't Empty";
+                                }else if(val!.length<5){
+                                  return "password must be 6 character or above";
                                 }
-                                if(!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z").hasMatch(value)){
-                                  return 'Please enter valid emial';
-                                }
-                                return null;
                               },
-                              onSaved:(dynamic email){
-                                _email=email ;
-                              },
-
-                              autofillHints: [AutofillHints.email],
+                              autofillHints: [AutofillHints.password],
                               cursorColor: Colors.red,
+                              obscureText: true,
                               decoration: InputDecoration(
                                 border: UnderlineInputBorder(),
-                                labelText: "Email",
+                                labelText: "Password",
                               ),
                             ),
                           ),
                         ],
                       ),
-                        ////////////2nd
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 14, 0, 0),
-                              child: Icon(
-                                Icons.lock,
-                                size: 17,
-                                color: Colors.blue,
-                              ),
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width * .80,
-                              padding: EdgeInsets.fromLTRB(10, 30, 10, 05),
-                              child: TextFormField(
+                      ],
+                    ),
+                    ),
 
-                                validator: (val) {
-                                  if(val ==""){
-                                    return "password Can't Empty";
-                                  }else if(val!.length<5){
-                                    return "password must be 6 character or above";
-                                  }
-                                },
-                                autofillHints: [AutofillHints.password],
-                                cursorColor: Colors.red,
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  border: UnderlineInputBorder(),
-                                  labelText: "Password",
-                                ),
-                              ),
-                            ),
-                          ],
+                    Container(
+                        alignment: Alignment.centerRight,
+                        height: MediaQuery.of(context).size.height * .05,
+                        width: MediaQuery.of(context).size.width,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Forgot the password?',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ))),
+                    Container(
+                        height: MediaQuery.of(context).size.height * .05,
+                        width: MediaQuery.of(context).size.width * .80,
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
                         ),
-                    ],
-                  ),
-                      )),
-
-                  Container(
-                      alignment: Alignment.centerRight,
-                      height: MediaQuery.of(context).size.height * .05,
-                      width: MediaQuery.of(context).size.width,
-                      child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Forgot the password?',
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
-                          ))),
-                  Container(
-                      height: MediaQuery.of(context).size.height * .05,
-                      width: MediaQuery.of(context).size.width * .80,
-                      decoration: BoxDecoration(
-                        color: Colors.blueAccent,
-                      ),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            _submit();
+                        child: ElevatedButton(
+                            onPressed: () {
+                              _submit();
 
 
-                          },
-                          child: Text('LOG IN'))),
-              Divider(height: 20,
-                thickness: 5,
-                indent: 20,
-                endIndent: 0,
-                color: Colors.black,),
-              Row(
-                children: [
-                  Divider( height: 20,
-                      thickness: 5,
-                      indent: 20,
-                      endIndent: 0,
-                      color: Colors.red,),
-                ],
-              )
-                ],
+                            },
+                            child: Text('LOG IN'))),
+                Divider(height: 20,
+                  thickness: 5,
+                  indent: 20,
+                  endIndent: 0,
+                  color: Colors.black,),
+                Row(
+                  children: [
+                    Divider( height: 20,
+                        thickness: 5,
+                        indent: 20,
+                        endIndent: 0,
+                        color: Colors.red,),
+                  ],
+                )
+                  ],
+                ),
               ),
             ),
           ),
